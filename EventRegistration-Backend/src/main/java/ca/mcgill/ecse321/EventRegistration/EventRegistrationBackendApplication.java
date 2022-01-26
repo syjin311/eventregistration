8b95fbe999ca179fe806334f262267e4e7814472
Youjin <syjin311@yonsei.ac.kr>
2022-01-25 21:31:45 -0500

    Demo

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/EventRegistration/EventRegistrationBackendApplication.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/EventRegistration/EventRegistrationBackendApplication.java
@@ -13,6 +13,7 @@ public class EventRegistrationBackendApplication {
     SpringApplication.run(EventRegistrationBackendApplication.class, args);
   }
 
+// Request map
   @RequestMapping("/")
   public String greeting(){
     return "Hello world!";
